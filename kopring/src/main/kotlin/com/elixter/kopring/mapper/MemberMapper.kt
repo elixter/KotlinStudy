9724ef1bd6736fcbb8cf8d93f8897cffae90e482
elixter <ltw971@naver.com>
2022-08-20 18:53:31 +0900

    feat: add findByid test

--- kopring/src/main/kotlin/com/elixter/kopring/mapper/MemberMapper.kt
+++ kopring/src/main/kotlin/com/elixter/kopring/mapper/MemberMapper.kt
@@ -3,6 +3,7 @@ package com.elixter.kopring.mapper
 import com.elixter.kopring.domain.Member
 import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -11,5 +12,5 @@ interface MemberMapper {
 
     fun save(member: Member)
 
-    fun findById(id: Long): Member?
+    fun findById(@Param("id") id: Long): Member?
 }
